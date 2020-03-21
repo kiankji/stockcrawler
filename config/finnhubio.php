ca18c6e1fd5fbcf2587ef4610180d1c6207aaee3
Kian <mail@kian-ansari.de>
2020-03-21 18:00:24 +0100

    -// init the project with laravel 7.* , finnhub api plugin and infyom

---
+++ config/finnhubio.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Finnhub.io Configuration file
+    |--------------------------------------------------------------------------
+    |
+    | Got to https://finnhub.io/docs/api to more details and setup your account
+    |
+    */
+
+    'token' => env('FINNHUB_API_KEY', 'API_KEY'),
+
+];
